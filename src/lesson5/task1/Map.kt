c32d9c33b2c904e64c48afccb7acd418c5cf1e7d
Duodenum <strongsem123@gmail.com>
2019-12-08 19:51:48 +0300

    hopeless

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -103,7 +103,7 @@ fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")) -> true
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
-fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = TODO()
+fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = (b + a) == b
 
 /**
  * Простая
@@ -119,7 +119,11 @@ fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = TODO()
  *   subtractOf(a = mutableMapOf("a" to "z"), mapOf("a" to "z"))
  *     -> a changes to mutableMapOf() aka becomes empty
  */
-fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Unit = TODO()
+fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Unit {
+    for ((keys, values) in b) {
+        if ((a[keys] == b[keys]) && (a[values] == b[values])) a.remove(keys)
+    }
+}
 
 /**
  * Простая
@@ -128,7 +132,13 @@ fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Unit = TO
  * В выходном списке не должно быть повторяюихся элементов,
  * т. е. whoAreInBoth(listOf("Марат", "Семён, "Марат"), listOf("Марат", "Марат")) == listOf("Марат")
  */
-fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = TODO()
+fun whoAreInBoth(a: List<String>, b: List<String>): List<String> {
+    var nameList = mutableListOf<String>()
+    for (name in b) {
+        if (a.contains(name)) nameList.add(name)
+    }
+    return nameList
+}
 
 /**
  * Средняя
